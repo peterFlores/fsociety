671a1139dd760f6cdcd0fd3099c8f624826db7f1
Celso Rojas <titijavier27@gmail.com>
2020-09-15 11:59:17 -0600

    PROFILE PICTURE PATH CHANGED, TEST UPLOAD PROFILEPICTURE

--- backend-spring-boot/social-app/src/main/java/com/redsocial/web/app/DAO/UserDAO.java
+++ backend-spring-boot/social-app/src/main/java/com/redsocial/web/app/DAO/UserDAO.java
@@ -104,12 +104,12 @@ public class UserDAO implements IUserService {
 
 		String sql = "SELECT * FROM USERS WHERE USER_ROLE = 'SOCIAL' ";
 
-		List<User> listUser = jdbcTemplate.query(sql,new RowMapper<User>() {
-			
+		List<User> listUser = jdbcTemplate.query(sql, new RowMapper<User>() {
+
 			@Override
 			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
 				User user = new User();
-				
+
 				user.setUserImage(rs.getString("USER_IMAGE_PATH"));
 				user.setIdUser(rs.getLong("USER_ID"));
 				user.setUserName(rs.getString("USER_NAME"));
@@ -124,20 +124,19 @@ public class UserDAO implements IUserService {
 		});
 
 		return listUser;
-		
-	
+
 	}
 
 	@Override
 	public List<User> findByUserId(Integer idUser) {
 		String sql = "SELECT * FROM USERS WHERE USER_ID = ?";
 
-		List<User> listUser = jdbcTemplate.query(sql,new Object[] {idUser},new RowMapper<User>() {
-			
+		List<User> listUser = jdbcTemplate.query(sql, new Object[] { idUser }, new RowMapper<User>() {
+
 			@Override
 			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
 				User user = new User();
-				
+
 				user.setUserImage(rs.getString("USER_IMAGE_PATH"));
 				user.setIdUser(rs.getLong("USER_ID"));
 				user.setUserName(rs.getString("USER_NAME"));
@@ -158,49 +157,41 @@ public class UserDAO implements IUserService {
 	public List<User> findByUserMail(String email) {
 		String sql = "SELECT * FROM USERS WHERE USER_MAIL = ?";
 
-		List<User> listUser = jdbcTemplate.query(sql,new Object[] {email},new RowMapper<User>(){
-		
-		@Override
-		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
-			User user = new User();
+		List<User> listUser = jdbcTemplate.query(sql, new Object[] { email }, new RowMapper<User>() {
 
-			user.setIdUser(rs.getLong("USER_ID"));
-			user.setUserName(rs.getString("USER_NAME"));
-			user.setUserNickname(rs.getString("USER_NICKNAME"));
-			user.setUserMail(rs.getString("USER_MAIL"));
-			user.setUserBirthDate(rs.getDate("USER_BIRTHDATE"));
-			user.setUserGender(rs.getString("USER_GENDER"));
-			user.setUserCreatedAt(rs.getDate("USER_CREATED_AT"));
-			user.setUserStatus(rs.getString("USER_STATUS"));
-			user.setUserImage(rs.getString("USER_IMAGE_PATH"));
-			user.setUserPassword(rs.getString("USER_PASSWORD"));
-			user.setUserRole(rs.getString("USER_ROLE"));
-			
-
-			
-			return user;
-		}
-	});
-			
-
-		return listUser;
-	}
-	
-
-	
-	
-	@Override
-	public List<User> findByUserNickname(String nickname) {
-		String sql = "SELECT * FROM USERS WHERE USER_NICKNAME LIKE  ?";
-		
-
-
-		List<User> listUser = jdbcTemplate.query(sql,new Object[] {nickname},new RowMapper<User>() {
-			
 			@Override
 			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
 				User user = new User();
-				
+
+				user.setIdUser(rs.getLong("USER_ID"));
+				user.setUserName(rs.getString("USER_NAME"));
+				user.setUserNickname(rs.getString("USER_NICKNAME"));
+				user.setUserMail(rs.getString("USER_MAIL"));
+				user.setUserBirthDate(rs.getDate("USER_BIRTHDATE"));
+				user.setUserGender(rs.getString("USER_GENDER"));
+				user.setUserCreatedAt(rs.getDate("USER_CREATED_AT"));
+				user.setUserStatus(rs.getString("USER_STATUS"));
+				user.setUserImage(rs.getString("USER_IMAGE_PATH"));
+				user.setUserPassword(rs.getString("USER_PASSWORD"));
+				user.setUserRole(rs.getString("USER_ROLE"));
+
+				return user;
+			}
+		});
+
+		return listUser;
+	}
+
+	@Override
+	public List<User> findByUserNickname(String nickname) {
+		String sql = "SELECT * FROM USERS WHERE USER_NICKNAME LIKE  ?";
+
+		List<User> listUser = jdbcTemplate.query(sql, new Object[] { nickname }, new RowMapper<User>() {
+
+			@Override
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+
 				user.setUserImage(rs.getString("USER_IMAGE_PATH"));
 				user.setIdUser(rs.getLong("USER_ID"));
 				user.setUserName(rs.getString("USER_NAME"));
@@ -216,18 +207,17 @@ public class UserDAO implements IUserService {
 
 		return listUser;
 	}
-	
-	
+
 	@Override
 	public List<User> findByUserName(String userName) {
 		String sql = "SELECT * FROM USERS WHERE USER_NAME = ?";
 
-		List<User> listUser = jdbcTemplate.query(sql,new Object[] {userName},new RowMapper<User>() {
-			
+		List<User> listUser = jdbcTemplate.query(sql, new Object[] { userName }, new RowMapper<User>() {
+
 			@Override
 			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
 				User user = new User();
-				
+
 				user.setUserImage(rs.getString("USER_IMAGE_PATH"));
 				user.setIdUser(rs.getLong("USER_ID"));
 				user.setUserName(rs.getString("USER_NAME"));
@@ -247,25 +237,26 @@ public class UserDAO implements IUserService {
 	@Override
 	public void updatePicture(MultipartFile picture, Long idUser) throws IllegalStateException, IOException {
 
-		String route = "/var/www/html/Images/";    
+		String saveRoute = "/var/www/html/Images/";
+		String route = "http://3.22.230.92/Images/Profile/";
 		User user = new User();
-		
+
+		File profilepic = new File(saveRoute + picture.getOriginalFilename());
+
+		picture.transferTo(profilepic);
+
 		user.setIdUser(idUser);
 		user.setUserImage(route + picture.getOriginalFilename());
-		
-			picture.transferTo(new File(route));
-		
-			
-			jdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("UPDATE_PICTURE_USER");
-			Map<String, Object> Map = new HashMap<String, Object>();
-			
-			Map.put("PUSER_ID", user.getIdUser());
-			Map.put("PUSER_IMAGE_PATH", user.getUserImage());
-	
-			SqlParameterSource src = new MapSqlParameterSource().addValues(Map);
-			jdbcCall.execute(src);
-			
-	}
 
+		jdbcCall = new SimpleJdbcCall(jdbcTemplate).withProcedureName("UPDATE_PICTURE_USER");
+		Map<String, Object> Map = new HashMap<String, Object>();
+
+		Map.put("PUSER_ID", user.getIdUser());
+		Map.put("PUSER_IMAGE_PATH", user.getUserImage());
+
+		SqlParameterSource src = new MapSqlParameterSource().addValues(Map);
+		jdbcCall.execute(src);
+
+	}
 
 }
