b59bfc91371330300cd76d19ab51abdabe023d3d
Pedro Flores <peter@Pedros-MacBook-Pro.local>
2020-09-15 20:12:05 -0600

    change requestmapping usercontroller

--- backend-spring-boot/social-app/src/main/java/com/redsocial/web/app/Controllers/UserController.java
+++ backend-spring-boot/social-app/src/main/java/com/redsocial/web/app/Controllers/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -23,8 +24,10 @@ import com.redsocial.web.app.Models.Response;
 import com.redsocial.web.app.Models.User;
 import com.redsocial.web.app.Services.IUserService;
 
+
 @RestController
 @CrossOrigin(origins = "*")
+@RequestMapping("user")
 public class UserController {
 
 	@Autowired
